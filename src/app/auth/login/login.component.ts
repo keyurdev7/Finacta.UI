16bcd105b54b148ae58e40858604f47269b9be11
Rahul Patel <its.merhp@gmail.com>
2022-10-15 16:20:46 +0530

    Stripe key api changes

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -4,7 +4,12 @@ import { APIService } from 'src/app/shared/services/api.service';
 import { Router } from '@angular/router';
 import { ToastrService } from 'ngx-toastr';
 import { Store } from '@ngrx/store';
-import { UpdateUserAction } from 'src/app/store/app.actions';
+import {
+  SetStripeKeyAction,
+  UpdateUserAction,
+} from 'src/app/store/app.actions';
+import { StripeKey } from 'src/app/models/stripe-key.model';
+import { AppState, stripeKeySelector } from 'src/app/store/app.state';
 
 @Component({
   selector: 'app-login',
@@ -15,13 +20,14 @@ export class LoginComponent implements OnInit {
   public show: boolean = false;
   public loginForm: FormGroup = new FormGroup([]);
   public errorMessage: any;
+  isStripeKeySet: boolean = false;
 
   constructor(
     private fb: FormBuilder,
     private api: APIService,
     public toster: ToastrService,
     public router: Router,
-    public store: Store
+    public store: Store<AppState>
   ) {
     document.querySelector('body')?.classList.add('login-img');
   }
@@ -31,6 +37,9 @@ export class LoginComponent implements OnInit {
       email: [null, [Validators.required, Validators.email]],
       password: [null, [Validators.required, Validators.minLength(8)]],
     });
+    this.store.pipe(stripeKeySelector).subscribe((res) => {
+      this.isStripeKeySet = !!res.key;
+    });
   }
 
   login() {
@@ -39,11 +48,19 @@ export class LoginComponent implements OnInit {
       .subscribe((res) => {
         if (res && res.succeeded) {
           this.loginForm.reset();
-          if(!!res.data.isTemporaryPassword){
-            this.router.navigate(['/auth/setpassword/'+res.data.userId]);
-          }
-          else{
+          if (!!res.data.isTemporaryPassword) {
+            this.router.navigate(['/auth/setpassword/' + res.data.userId]);
+          } else {
             this.store.dispatch(UpdateUserAction(res.data));
+            if (!this.isStripeKeySet) {
+              this.api.getStripeKey().subscribe((res2) => {
+                if (res2 && res2.succeeded) {
+                  const stripeKey = new StripeKey();
+                  stripeKey.key = res2.data;
+                  this.store.dispatch(SetStripeKeyAction(stripeKey));
+                }
+              });
+            }
             if (!!res.data.isPortalSubscibe) {
               this.router.navigate(['/dashboard']);
             } else {
