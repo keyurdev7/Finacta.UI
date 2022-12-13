4a0b8caa0cf9da9dbe434ef172ea34fce3059201
Rahul Patel <its.merhp@gmail.com>
2022-12-14 00:45:25 +0530

    Conflict resolved

--- src/app/shared/layout-components/header/header.component.ts
+++ src/app/shared/layout-components/header/header.component.ts
@@ -121,6 +121,10 @@ export class HeaderComponent implements OnInit, OnDestroy {
     return !!text ? text.trim() : '';
   }
 
+  getCompantText(text: string): string {
+    return !!text ? text.trim() : '';
+  }
+
   reload() {
     window.location.reload();
   }
